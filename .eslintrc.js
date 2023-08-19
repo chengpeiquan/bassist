910b8cba0fb6dd54d6598e331725761fefb9ed39
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-08-19 16:50:02 +0800

    chore: typo

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
     'prettier/prettier': 'warn',
   },
 }
