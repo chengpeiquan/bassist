3cfbf6a75a1d8763a6e4ed0da20e9ef08b9e77e8
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-08-19 22:40:06 +0800

    feat(eslint): add eslint config

---
+++ packages/eslint/src/index.ts
@@ -0,0 +1,7 @@
+export * from './define'
+export * from './javascript'
+export * from './markdown'
+export * from './prettier'
+export * from './typescript'
+export * from './unocss'
+export * from './vue'
