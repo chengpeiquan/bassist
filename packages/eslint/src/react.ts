3f4b5311b25b9823f28439e318425348940662eb
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-08-24 00:16:07 +0800

    fix(eslint): fix some misconfigurations for React rules

--- packages/eslint/src/react.ts
+++ packages/eslint/src/react.ts
@@ -1,18 +1,33 @@
 import globals from 'globals'
 import reactPlugin from 'eslint-plugin-react'
 import reactHooksConfig from 'eslint-plugin-react-hooks'
+import { typescript, tsParser, tsPlugin } from './typescript'
 import { GLOB_JS, GLOB_JSX, GLOB_TS, GLOB_TSX } from './constants'
-import type { FlatESLintConfigItem } from 'eslint-define-config'
+import type { FlatESLintConfigItem, Rules } from 'eslint-define-config'
+
+const reactCustomRules: Rules = {
+  'react/jsx-uses-react': 'error',
+  'react/jsx-uses-vars': 'error',
+}
 
 export const react: FlatESLintConfigItem[] = [
   {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     files: [GLOB_JS, GLOB_JSX, GLOB_TS, GLOB_TSX],
     plugins: {
       react: reactPlugin,
-      reactHooks: reactHooksConfig,
+      'react-hooks': reactHooksConfig,
+      '@typescript-eslint': tsPlugin,
     },
     languageOptions: {
+      ecmaVersion: 'latest',
+      parser: tsParser,
       parserOptions: {
+        sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
@@ -22,8 +37,10 @@ export const react: FlatESLintConfigItem[] = [
       },
     },
     rules: {
+      ...typescript[0].rules,
       ...reactPlugin.configs.recommended.rules,
       ...reactHooksConfig.configs.recommended.rules,
+      ...reactCustomRules,
     },
   },
 ]
