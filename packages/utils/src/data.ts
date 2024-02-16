03e3dee0d1954ef57359f51ec0d51df843b5973b
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-02-16 15:04:27 +0800

    feat(utils): add isXX methods to check whether the data is of the specified type

--- packages/utils/src/data.ts
+++ packages/utils/src/data.ts
@@ -18,8 +18,8 @@ export type DataType =
   | 'error'
   | 'set'
   | 'map'
-  | 'weakSet'
-  | 'weakMap'
+  | 'weakset'
+  | 'weakmap'
   | 'file'
   | 'blob'
   | 'arraybuffer'
@@ -27,6 +27,9 @@ export type DataType =
 
 /**
  * Get the real data type
+ *
+ * @description Solve the judgment error of the value wrapped in an Object
+ *  e.g. Object(1n), Object(1)
  *
  * @category data
  */
@@ -38,12 +41,183 @@ export function getDataType(target: any) {
 }
 
 /**
- * Determine whether the data is an object
+ * Determine whether the data is boolean
  *
  * @category data
  */
-export function isObject(target: any) {
-  return getDataType(target) === 'object'
+export function isBoolean(value: unknown): value is boolean {
+  return getDataType(value) === 'boolean'
+}
+
+/**
+ * Determine whether the data is string
+ *
+ * @category data
+ */
+export function isString(value: unknown): value is string {
+  return getDataType(value) === 'string'
+}
+
+/**
+ * Determine whether the data is number
+ *
+ * @category data
+ */
+export function isNumber(value: unknown): value is number {
+  return getDataType(value) === 'number'
+}
+
+/**
+ * Determine whether the data is bigint
+ *
+ * @category data
+ */
+export function isBigInt(value: unknown): value is bigint {
+  return getDataType(value) === 'bigint'
+}
+
+/**
+ * Determine whether the data is symbol
+ *
+ * @category data
+ */
+export function isSymbol(value: unknown): value is symbol {
+  return getDataType(value) === 'symbol'
+}
+
+/**
+ * Determine whether the data is null
+ *
+ * @category data
+ */
+export function isNull(value: unknown): value is null {
+  return getDataType(value) === 'null'
+}
+
+/**
+ * Determine whether the data is undefined
+ *
+ * @category data
+ */
+export function isUndefined(value: unknown): value is undefined {
+  return getDataType(value) === 'undefined'
+}
+
+/**
+ * Determine whether the data is function
+ *
+ * @category data
+ */
+export function isFunction(value: unknown): value is (...args: any) => any {
+  return getDataType(value) === 'function'
+}
+
+/**
+ * Determine whether the data is object
+ *
+ * @category data
+ */
+export function isObject(value: unknown): value is Record<any, any> {
+  return getDataType(value) === 'object'
+}
+
+/**
+ * Determine whether the data is array
+ *
+ * @category data
+ */
+export function isArray(value: unknown): value is any[] {
+  return getDataType(value) === 'array'
+}
+
+/**
+ * Determine whether the data is date
+ *
+ * @category data
+ */
+export function isDate(value: unknown): value is Date {
+  return getDataType(value) === 'date'
+}
+
+/**
+ * Determine whether the data is error
+ *
+ * @category data
+ */
+export function isError(value: unknown): value is Error {
+  return getDataType(value) === 'error'
+}
+
+/**
+ * Determine whether the data is set
+ *
+ * @category data
+ */
+export function isSet(value: unknown): value is Set<any> {
+  return getDataType(value) === 'set'
+}
+
+/**
+ * Determine whether the data is map
+ *
+ * @category data
+ */
+export function isMap(value: unknown): value is Map<any, any> {
+  return getDataType(value) === 'map'
+}
+
+/**
+ * Determine whether the data is weakset
+ *
+ * @category data
+ */
+export function isWeakSet(value: unknown): value is WeakSet<WeakKey> {
+  return getDataType(value) === 'weakset'
+}
+
+/**
+ * Determine whether the data is weakmap
+ *
+ * @category data
+ */
+export function isWeakMap(value: unknown): value is WeakMap<WeakKey, any> {
+  return getDataType(value) === 'weakmap'
+}
+
+/**
+ * Determine whether the data is file
+ *
+ * @category data
+ */
+export function isFile(value: unknown): value is File {
+  return getDataType(value) === 'file'
+}
+
+/**
+ * Determine whether the data is blob
+ *
+ * @category data
+ */
+export function isBlob(value: unknown): value is Blob {
+  return getDataType(value) === 'blob'
+}
+
+/**
+ * Determine whether the data is arraybuffer
+ *
+ * @category data
+ */
+export function isArrayBuffer(value: unknown): value is ArrayBuffer {
+  return getDataType(value) === 'arraybuffer'
+}
+
+/**
+ * Determine whether the data is regexp
+ *
+ * @category data
+ */
+export function isRegExp(value: unknown): value is RegExp {
+  return getDataType(value) === 'regexp'
 }
 
 /**
@@ -51,7 +225,7 @@ export function isObject(target: any) {
  *
  * @category data
  */
-export function hasKey(obj: Record<string, any>, key: string) {
+export function hasKey<T, K extends keyof T>(obj: T, key: K): key is K {
   if (!isObject(obj)) return false
   return Object.prototype.hasOwnProperty.call(obj, key)
 }
