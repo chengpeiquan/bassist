4ee1d92b2ecd6417bd00b5130e236cdcceceb8fe
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-02-24 20:32:35 +0800

    feat(utils): adjust the judgment of isFunction to typeof, and add isAsyncFunction

--- packages/utils/test/data.spec.ts
+++ packages/utils/test/data.spec.ts
@@ -1,15 +1,26 @@
 import { describe, expect, it } from 'vitest'
-import { getDataType, isObject, isArray, inRange } from '..'
+import {
+  getDataType,
+  isObject,
+  isArray,
+  inRange,
+  isFunction,
+  isAsyncFunction,
+} from '..'
+
+class Foo {
+  bar() {}
+}
 
 describe('getDataType', () => {
   it('Valid data', () => {
-    expect(getDataType('')).toBe('string')
-    expect(getDataType(String(1))).toBe('string')
-    expect(getDataType(new String(1))).toBe('string')
-    expect(getDataType(null)).toBe('null')
-    expect(getDataType(undefined)).toBe('undefined')
-    expect(getDataType(new Date())).toBe('date')
-    expect(getDataType(/foo/)).toBe('regexp')
+    expect(getDataType('')).toBe('String')
+    expect(getDataType(String(1))).toBe('String')
+    expect(getDataType(new String(1))).toBe('String')
+    expect(getDataType(null)).toBe('Null')
+    expect(getDataType(undefined)).toBe('Undefined')
+    expect(getDataType(new Date())).toBe('Date')
+    expect(getDataType(/foo/)).toBe('RegExp')
   })
 })
 
@@ -28,6 +39,40 @@ describe('isArray', () => {
     expect(isArray(null)).toBeFalsy()
     expect(isArray(undefined)).toBeFalsy()
     expect(isArray(Object(1))).toBeFalsy()
+  })
+})
+
+describe('isFunction', () => {
+  it('Valid data', () => {
+    expect(isFunction(new Function())).toBeTruthy()
+    expect(isFunction(function () {})).toBeTruthy()
+    expect(isFunction(() => {})).toBeTruthy()
+    expect(isFunction(async () => {})).toBeTruthy()
+    expect(isFunction(async function () {})).toBeTruthy()
+    expect(isFunction(Foo)).toBeTruthy()
+    expect(isFunction(new Foo().bar)).toBeTruthy()
+  })
+  it('Invalid data', () => {
+    expect(isFunction(1)).toBeFalsy()
+    expect(isFunction('1')).toBeFalsy()
+    expect(isFunction(undefined)).toBeFalsy()
+    expect(isFunction(null)).toBeFalsy()
+    expect(isFunction({})).toBeFalsy()
+    expect(isFunction([])).toBeFalsy()
+  })
+})
+
+describe('isAsyncFunction', () => {
+  it('Valid data', () => {
+    expect(isAsyncFunction(async () => {})).toBeTruthy()
+    expect(isAsyncFunction(async function () {})).toBeTruthy()
+  })
+  it('Invalid data', () => {
+    expect(isAsyncFunction(new Function())).toBeFalsy()
+    expect(isAsyncFunction(function () {})).toBeFalsy()
+    expect(isAsyncFunction(() => {})).toBeFalsy()
+    expect(isAsyncFunction(Foo)).toBeFalsy()
+    expect(isAsyncFunction(new Foo().bar)).toBeFalsy()
   })
 })
 
