57f50ca0768a5626584593fb0e6d9c6bccd176d6
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-03-09 02:24:22 +0800

    chore(eslint): deprecated

--- packages/eslint/src/define.ts
+++
@@ -1,9 +0,0 @@
-import { defineFlatConfig } from 'eslint-define-config'
-import type { FlatESLintConfig, Rules } from 'eslint-define-config'
-
-export {
-  defineFlatConfig,
-  defineFlatConfig as defineConfig, // Alias
-  type FlatESLintConfig,
-  type Rules,
-}
