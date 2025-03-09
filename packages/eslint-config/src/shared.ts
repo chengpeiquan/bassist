ffa4046914d9337deb251154bd8923ab89a597f4
chengpeiquan <chengpeiquan@chengpeiquan.com>
2025-03-09 23:33:56 +0800

    fix(eslint-config): export createGetConfigNameFactory

--- packages/eslint-config/src/shared.ts
+++ packages/eslint-config/src/shared.ts
@@ -1,5 +1,23 @@
-const prefix = 'bassist'
+const defaultPrefix = 'bassist'
 
-export const getConfigName = (...names: string[]): string => {
-  return `${prefix}/${names.join('/')}`
+/**
+ * A flexible tool function for generating ESLint configuration naming tools. It
+ * helps you quickly splice configuration names, ensure consistent namespaces,
+ * and facilitate the organization and management of complex rule sets.
+ *
+ * @param prefix - A string representing the prefix for your configuration
+ *   names.
+ * @returns A function that concatenates the provided name segments with the
+ *   given prefix.
+ */
+export const createGetConfigNameFactory = (prefix: string) => {
+  const getConfigName = (...names: string[]): string => {
+    const finalPrefix = prefix?.trim() || defaultPrefix
+    return `${finalPrefix}/${names.join('/')}`
+  }
+
+  return getConfigName
 }
+
+// Provided for internal configuration
+export const getConfigName = createGetConfigNameFactory(defaultPrefix)
