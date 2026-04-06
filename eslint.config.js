a6afde0f3a77b8a0dd9bd8b36f81c50c08dd4d08
chengpeiquan <chengpeiquan@chengpeiquan.com>
2026-04-06 23:48:40 +0800

    fix: restore ESLint configurations that use the .js extension to resolve not working in the editor

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
+// @ts-check
+
 import { defineEslintConfig, eslintPresets } from '@bassist/oxc-integration'
 
 export default defineEslintConfig(
   eslintPresets.node(),
   eslintPresets.vitest(),
+  eslintPresets.imports(),
   {
     name: '@bassist/monorepo/ignore',
     ignores: ['**/dist/**', '**/.build/**', '**/CHANGELOG.md'],
